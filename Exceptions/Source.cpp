ab8f90065de30a963c40eabcb24ed1241d8e1b0e
Snowy-Cuber <tfischerrenee1@gmail.com>
2025-09-17 22:49:11 -0400

    Completed outline of program.

--- Exceptions/Source.cpp
+++ Exceptions/Source.cpp
@@ -3,18 +3,52 @@
 // 9/17/2025
 
 #include<iostream>
-
+#include<cstring>
+#include<cctype>
 using namespace std;
 
+char character(char, int);
 
 int main() {
 
+	try {
 
+		cout << character('a', 1);
 
+	}
+	catch () {
+
+	}
+	catch (){
+
+	}
 
 
 
 	cout << endl << endl;
 	system("pause");
 	return 0;
+}
+
+
+char character(char start, int offset) {
+
+	char newCharacter = start;
+
+	if (!isalpha(start)) {
+		throw;
+	}
+
+	if (offset < 1 || offset >= 32) {
+		throw;
+	}
+
+	newCharacter += offset;
+
+	if (!isalpha(newCharacter)) {
+		throw;
+	}
+
+	return newCharacter;
+
 }
